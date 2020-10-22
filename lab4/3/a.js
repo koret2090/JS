453b7d5d4846dbc95a98a71c90fd6cf5e8141543
Suslikov Daniil <danyvs@mail.ru>
2020-10-22 20:09:36 +0300

    move_files

--- lab4/3/a.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const x = "" + process.argv[2];
-const y = "" + process.argv[3];
-const result = parseInt(x) + parseInt(y);
-console.log("" + result);
--- lab4/3/b.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const x = "" + process.argv[2];
-const result = parseInt(x) * 2;
-console.log("" + result);
